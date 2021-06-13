badb0637a5417ca1d68d331df7ccc2eee8ff2586
CristianAUnisa <c.alfano35@studenti.unisa.it>
2021-06-13 18:57:42 +0200

    Dark theme

---
+++ assets/javascript/main.js
@@ -0,0 +1,58 @@
+// Dark/Light color scheme switch button
+document.querySelector("#nav-switch-theme").addEventListener("click", changeColorScheme);
+
+function changeColorScheme() {
+  $("#icon-theme").toggleClass("fa-sun fa-moon");
+  // Use whatever users want
+  if (localStorage.getItem("colorScheme") === "dark") {
+    // Change to light theme
+    if (window.matchMedia("(prefers-color-scheme: dark)").matches === false) {
+      document.querySelector("#dark-css").setAttribute("media", "(prefers-color-scheme: dark)");
+      localStorage.removeItem("colorScheme");
+    } else {
+      // by setting invalid media it will just not apply CSS for anyone
+      document.querySelector("#dark-css").setAttribute("media", "invalid");
+      localStorage.setItem("colorScheme", "light");
+    }
+  }
+  // Change to dark theme
+  else if (localStorage.getItem("colorScheme") === "light") {
+    if (window.matchMedia("(prefers-color-scheme: dark)").matches === true) {
+      document.querySelector("#dark-css").setAttribute("media", "(prefers-color-scheme: dark)");
+      localStorage.removeItem("colorScheme");
+    } else {
+      // media was set to prefers-color-scheme: dark
+      document.querySelector("#dark-css").removeAttribute("media");
+      localStorage.setItem("colorScheme", "dark");
+    }
+  }
+
+  // Just use whatever browsers want
+  else if (window.matchMedia("(prefers-color-scheme: dark)").matches === true) {
+    // Change to light Theme
+    document.querySelector("#dark-css").setAttribute("media", "invalid");
+    localStorage.setItem("colorScheme", "light");
+  } else {
+    // Change to dark theme
+    document.querySelector("#dark-css").removeAttribute("media");
+    localStorage.setItem("colorScheme", "dark");
+  }
+  fixThemeImages();
+}
+
+
+// Fix images in dark theme
+function fixThemeImages() {
+  document.querySelectorAll('[data-theme-src]').forEach(function(image) {
+    tempSrc = image.src;
+    image.src = image.getAttribute("data-theme-src");
+    image.setAttribute("data-theme-src", tempSrc);
+  });
+}
+if (
+  (localStorage.getItem("colorScheme") === "dark") ||
+  (window.matchMedia("(prefers-color-scheme: dark)").matches ^
+    localStorage.getItem("colorScheme") === "light")
+) {
+  fixThemeImages();
+}
